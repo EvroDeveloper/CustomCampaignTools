c98db06f34a71a03fe408d3eb9ce66627cacb02d
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-01-29 15:11:22 -0500

    SavePoint box collection functionality

--- CustomCampaignTools/MonoBehaviours/SavePoint.cs
+++ CustomCampaignTools/MonoBehaviours/SavePoint.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using MelonLoader;
+using Il2CppSLZ.Marrow.Interaction;
+using Il2CppSLZ.Marrow;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CustomCampaignTools.SDK
 {
@@ -12,7 +16,30 @@ namespace CustomCampaignTools.SDK
         public void Save()
         {
             string barcode = SceneStreamer.Session.Level.Barcode.ID;
-            SavepointFunctions.SavePlayer(barcode, transform.position, transform.position);
+
+            if(TryGetComponent(out BoxCollider collider))
+            {
+                HashSet<MarrowEntity> boxVolEntities = new HashSet<MarrowEntity>();
+
+                Collider[] trackers = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, Quaternion.identity, LayerMask.GetMask("EntityTracker"));
+                foreach (Collider tracker in trackers)
+                {
+                    if(tracker.TryGetComponent(out Tracker t))
+                    {
+                        boxVolEntities.Add(t.entity);
+                    }
+                }
+
+                List<string> entityBarcodes = new List<string>();
+                foreach(MarrowEntity entity in boxVolEntities)
+                {
+                    entityBarcodes.Add(entity.Poolee.Spawnable.Barcode);
+                }
+                SavepointFunctions.SavePlayer(barcode, transform.position, collider.bounds.center, entityBarcodes)
+            }
+            else {
+                SavepointFunctions.SavePlayer(barcode, transform.position, transform.position);
+            }
         }
     }
 }
