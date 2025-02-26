3b78cf1501885e37c20191754d691889e2e8c705
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-02-26 13:09:40 -0500

    Update Build Info

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -2,10 +2,10 @@
 {
     internal class BuildInfo
     {
-        internal const string Name = "Labworks_Addon";
-        internal const string Description = "Adds extra content from Boneworks to Labworks!";
-        internal const string Author = "Evro";
-        internal const string Company = "Labworks";
+        internal const string Name = "Custom Campain Tools";
+        internal const string Description = "Allows Modders to add their own Campaign functionality to their Marrow Campaigns.";
+        internal const string Author = "EvroDev";
+        internal const string Company = "LabWorks";
         internal const string Version = "1.0.0";
         internal const string DownloadLink = "Link";
     }
