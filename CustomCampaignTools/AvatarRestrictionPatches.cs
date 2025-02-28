220e81029afefb4951b4741ce2f723bb1bd290f0
EvroCraft <evro.minecraft@gmail.com>
2025-02-28 17:21:32 -0500

    Add stuff for updater app bc i am slow

--- CustomCampaignTools/AvatarRestrictionPatches.cs
+++ CustomCampaignTools/AvatarRestrictionPatches.cs
@@ -1,6 +1,10 @@
 using HarmonyLib;
 using UnityEngine;
 using Il2CppSLZ.Bonelab;
+using Il2CppSLZ.UI;
+using BoneLib.Notifications;
+using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Warehouse;
 
 namespace CustomCampaignTools.Patching
 {
@@ -27,6 +31,28 @@ namespace CustomCampaignTools.Patching
             if(!Campaign.SessionActive || !Campaign.Session.RestrictAvatar || Campaign.Session.saveData.AvatarUnlocked) return;
 
             __instance.Deactivate();
+            __instance.popUpMenu.Deactivate();
+
+            Notifier.Send(new Notification()
+            {
+                Title = Campaign.Session.Name,
+                Message = "Avatar switching is locked on a first playthrough",
+                Type = NotificationType.Error,
+                ShowTitleOnPopup = true,
+            });
+        }
+    }
+
+    [HarmonyPatch(typeof(RigManager._SwapAvatarCrate_d__66))]
+    public static class ForceAvatarSwitch
+    {
+        [HarmonyPatch(nameof(RigManager._SwapAvatarCrate_d__66.MoveNext))]
+        [HarmonyPrefix]
+        public static void OnAvatarSwapped(RigManager._SwapAvatarCrate_d__66 __instance)
+        {
+            if (!Campaign.SessionActive || !Campaign.Session.RestrictAvatar || Campaign.Session.saveData.AvatarUnlocked) return;
+
+            __instance.barcode = new Barcode(Campaign.Session.CampaignAvatar);
         }
     }
 }
