c064bd92d6439955c10d2616a40a68f7529cea2d
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-04-22 10:52:19 -0400

    WIP Restructure 7

--- CustomCampaignTools/Data/CampaignLevel.cs
+++ CustomCampaignTools/Data/CampaignLevel.cs
@@ -10,13 +10,48 @@ namespace CustomCampaignTools
         public Campaign campaign;
 
         public Barcode levelBarcode;
-        public string levelName;
+        public string levelName
+        {
+            get{
+                if(overrideName == string.Empty) return crate.Title;
+                return overrideName;
+            }
+        };
+
+        private string overrideName = "";
+
+        public CampaignLevelType type;
+
+        public LevelCrate crate
+        {
+            get {
+                if(_crate != null) return _crate;
+                MarrowGame.assetWarehouse.TryGetCrate(levelBarcode, out _crate);
+                return _crate;
+            }
+        }
+
+        private LevelCrate _crate;
 
         public bool isUnlocked
         {
             get {
                 return !campaign.LockLevelsUntilEntered || campaign.saveData.UnlockedLevels.Contains(barcode);
             }
+        }
+
+        public CampaignLevel(string barcode, string name, CampaignLevelType type)
+        {
+            levelBarcode = new Barcode(barcode);
+            overrideName = name;
+            this.type = type;
+        }
+
+        public CampaignLevel(SerializedLevelSetup levelSetup, CampaignLevelType type)
+        {
+            levelBarcode = new Barcode(levelSetup.levelBarcode);
+            overrideName = levelSetup.levelName;
+            this.type = type;
         }
     }
 
@@ -35,6 +70,11 @@ namespace CustomCampaignTools
         public static List<string> ToNames(this List<CampaignLevel> list)
         {
             return list.Select(c => c.levelName);
+        }
+
+        public static List<LevelCrate> ToCrates(this List<CampaignLevel> list)
+        {
+            return list.Select(c => c.crate).Where(c => c != null);
         }
     }
 }
