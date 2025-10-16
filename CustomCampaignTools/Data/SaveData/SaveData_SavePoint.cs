c263aec5ad6eb929a7df170172f595affcbb5e68
EvroDev <evro.minecraft@gmail.com>
2025-10-16 17:09:17 -0500

    Update SaveData_SavePoint.cs

--- CustomCampaignTools/Data/SaveData/SaveData_SavePoint.cs
+++ CustomCampaignTools/Data/SaveData/SaveData_SavePoint.cs
@@ -60,135 +60,135 @@ namespace CustomCampaignTools
             CampaignBoneMenu.RefreshCampaignPage(campaign);
 
         }
-    }
 
-    public struct SavePoint
-    {
-        public string LevelBarcode;
-        public float PositionX;
-        public float PositionY;
-        public float PositionZ;
-
-        public InventoryData InventoryData;
-        public AmmoSave MidLevelAmmoSave;
-        public List<BarcodePosRot> BoxContainedBarcodes;
-        public List<string> DespawnedSpawners;
-        public Dictionary<int, bool> ObjectEnabledSaves;
-
-        public SavePoint()
+        public struct SavePoint
         {
+            public string LevelBarcode;
+            public float PositionX;
+            public float PositionY;
+            public float PositionZ;
 
+            public InventoryData InventoryData;
+            public AmmoSave MidLevelAmmoSave;
+            public List<BarcodePosRot> BoxContainedBarcodes;
+            public List<string> DespawnedSpawners;
+            public Dictionary<int, bool> ObjectEnabledSaves;
+
+            public SavePoint()
+            {
+
+            }
+
+            public SavePoint(string levelBarcode, Vector3 position, InventoryData inventoryData, AmmoSave ammoSave, List<BarcodePosRot> boxContainedBarcodes, List<string> savedDespawns, Dictionary<int, bool> savedEnableds)
+            {
+                LevelBarcode = levelBarcode;
+                PositionX = position.x;
+                PositionY = position.y;
+                PositionZ = position.z;
+
+                InventoryData = inventoryData;
+                MidLevelAmmoSave = ammoSave;
+
+                BoxContainedBarcodes = boxContainedBarcodes;
+                DespawnedSpawners = savedDespawns;
+                ObjectEnabledSaves = savedEnableds;
+            }
+
+            /// <summary>
+            /// Returns true if the save point has a level barcode.
+            /// </summary>
+            /// <param name="out hasSpawnPoint"></param>
+            /// <returns></returns>
+            public bool IsValid(out bool hasSpawnPoint)
+            {
+                if (GetPosition() == Vector3.zero)
+                    hasSpawnPoint = false;
+                else
+                    hasSpawnPoint = true;
+
+                if (LevelBarcode == null || LevelBarcode == string.Empty)
+                    return false;
+
+                return true;
+            }
+
+            public void LoadContinue(Campaign campaign)
+            {
+                LoadContinue(new Barcode(campaign.LoadScene));
+            }
+
+            public void LoadContinue(Barcode loadScene)
+            {
+                if (!IsValid(out _)) return;
+
+                SavepointFunctions.WasLastLoadByContinue = true;
+
+                FadeLoader.Load(new Barcode(LevelBarcode), loadScene);
+            }
+
+            public bool GetEnabledStateFromID(int id, bool defaultEnabled)
+            {
+                if (ObjectEnabledSaves.Keys.Contains(id))
+                    return ObjectEnabledSaves[id];
+                else
+                    return defaultEnabled;
+            }
+
+            public Vector3 GetPosition()
+            {
+                return new Vector3(PositionX, PositionY, PositionZ);
+            }
         }
 
-        public SavePoint(string levelBarcode, Vector3 position, InventoryData inventoryData, AmmoSave ammoSave, List<BarcodePosRot> boxContainedBarcodes, List<string> savedDespawns, Dictionary<int, bool> savedEnableds)
+        public struct BarcodePosRot
         {
-            LevelBarcode = levelBarcode;
-            PositionX = position.x;
-            PositionY = position.y;
-            PositionZ = position.z;
+            public string barcode;
 
-            InventoryData = inventoryData;
-            MidLevelAmmoSave = ammoSave;
+            public float x;
+            public float y;
+            public float z;
 
-            BoxContainedBarcodes = boxContainedBarcodes;
-            DespawnedSpawners = savedDespawns;
-            ObjectEnabledSaves = savedEnableds;
-        }
+            public float qX;
+            public float qY;
+            public float qZ;
+            public float qW;
 
-        /// <summary>
-        /// Returns true if the save point has a level barcode.
-        /// </summary>
-        /// <param name="out hasSpawnPoint"></param>
-        /// <returns></returns>
-        public bool IsValid(out bool hasSpawnPoint)
-        {
-            if (GetPosition() == Vector3.zero)
-                hasSpawnPoint = false;
-            else
-                hasSpawnPoint = true;
+            public float sX;
+            public float sY;
+            public float sZ;
 
-            if (LevelBarcode == null || LevelBarcode == string.Empty)
-                return false;
+            public BarcodePosRot(Barcode barcode, Vector3 position, Quaternion rotation, Vector3 scale)
+            {
+                this.barcode = barcode.ID;
 
-            return true;
-        }
+                x = position.x;
+                y = position.y;
+                z = position.z;
 
-        public void LoadContinue(Campaign campaign)
-        {
-            LoadContinue(new Barcode(campaign.LoadScene));
-        }
+                qX = rotation.x;
+                qY = rotation.y;
+                qZ = rotation.z;
+                qW = rotation.w;
 
-        public void LoadContinue(Barcode loadScene)
-        {
-            if (!IsValid(out _)) return;
+                sX = scale.x;
+                sY = scale.y;
+                sZ = scale.z;
+            }
 
-            SavepointFunctions.WasLastLoadByContinue = true;
+            public Vector3 GetPosition()
+            {
+                return new Vector3(x, y, z);
+            }
 
-            FadeLoader.Load(new Barcode(LevelBarcode), loadScene);
-        }
+            public Quaternion GetRotation()
+            {
+                return new Quaternion(qX, qY, qZ, qW);
+            }
 
-        public bool GetEnabledStateFromID(int id, bool defaultEnabled)
-        {
-            if (ObjectEnabledSaves.Keys.Contains(id))
-                return ObjectEnabledSaves[id];
-            else
-                return defaultEnabled;
-        }
-
-        public Vector3 GetPosition()
-        {
-            return new Vector3(PositionX, PositionY, PositionZ);
-        }
-    }
-
-    public struct BarcodePosRot
-    {
-        public string barcode;
-
-        public float x;
-        public float y;
-        public float z;
-
-        public float qX;
-        public float qY;
-        public float qZ;
-        public float qW;
-
-        public float sX;
-        public float sY;
-        public float sZ;
-
-        public BarcodePosRot(Barcode barcode, Vector3 position, Quaternion rotation, Vector3 scale)
-        {
-            this.barcode = barcode.ID;
-
-            x = position.x;
-            y = position.y;
-            z = position.z;
-
-            qX = rotation.x;
-            qY = rotation.y;
-            qZ = rotation.z;
-            qW = rotation.w;
-
-            sX = scale.x;
-            sY = scale.y;
-            sZ = scale.z;
-        }
-
-        public Vector3 GetPosition()
-        {
-            return new Vector3(x, y, z);
-        }
-
-        public Quaternion GetRotation()
-        {
-            return new Quaternion(qX, qY, qZ, qW);
-        }
-
-        public Vector3 GetScale()
-        {
-            return new Vector3(sX, sY, sZ);
+            public Vector3 GetScale()
+            {
+                return new Vector3(sX, sY, sZ);
+            }
         }
     }
 }
