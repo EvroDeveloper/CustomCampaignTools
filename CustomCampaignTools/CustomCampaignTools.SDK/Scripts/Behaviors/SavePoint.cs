ee5eb9be5974bbec91776ab25d5b78e446488c6f
EvroDev <evro.minecraft@gmail.com>
2026-08-28 00:48:36 -0500

    Use proper box collider rotation in OverlapBox for SavePoints. Ideally make this a zone in the future and query any overlapping entities but ehh i dont care yet lmao

--- CustomCampaignTools/CustomCampaignTools.SDK/Scripts/Behaviors/SavePoint.cs
+++ CustomCampaignTools/CustomCampaignTools.SDK/Scripts/Behaviors/SavePoint.cs
@@ -57,7 +57,9 @@ namespace CustomCampaignTools.SDK
 
                 BoxCollider collider = entitySaveZone.Get();
 
-                Collider[] trackers = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, Quaternion.identity, (int)BoneLib.GameLayers.ENTITY_TRACKER, QueryTriggerInteraction.Collide);
+                Vector3 colliderScale = collider.transform.lossyScale;
+                Vector3 halfExtents = Vector3.Scale(collider.size * 0.5f, new Vector3(Mathf.Abs(colliderScale.x), Mathf.Abs(colliderScale.y), Mathf.Abs(colliderScale.z)));
+                Collider[] trackers = Physics.OverlapBox(collider.transform.TransformPoint(collider.center), halfExtents, collider.transform.rotation, (int)BoneLib.GameLayers.ENTITY_TRACKER, QueryTriggerInteraction.Collide);
                 foreach (Collider tracker in trackers)
                 {
                     if (tracker.TryGetComponent(out Tracker t))
